edc16b814bcc5f98f209a579fbdb0034c214b863
mateenunez <mateenunez@gmail.com>
2025-07-18 12:33:32 -0300

    fix sitemap

--- app/sitemap.xml.ts
+++ app/sitemap.xml.ts
@@ -1,29 +1,12 @@
-import { type NextRequest } from "next/server";
-
-export async function GET(req: NextRequest) {
-  const baseUrl = "https://f1telemetry.vercel.app/";
-  const urls = [
-    "",
-  ];
-
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${urls
-    .map(
-      (url) => `
-    <url>
-      <loc>${baseUrl}${url}</loc>
-      <changefreq>daily</changefreq>
-      <priority>1.0</priority>
-    </url>
-  `
-    )
-    .join("")}
-</urlset>`;
-
-  return new Response(sitemap, {
-    headers: {
-      "Content-Type": "application/xml",
-    },
-  });
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://f1telemetry.vercel.app/',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    }
+  ]
 }
