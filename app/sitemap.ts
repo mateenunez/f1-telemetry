4237e85fbb637005745953bf05647f06b92cab5a
mateenunez <mateenunez@gmail.com>
2025-09-30 19:22:39 -0300

    sitemap change

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,13 +3,13 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://f1telemetry.vercel.app/',
+      url: 'https://f1telemetry.com/',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1.0,
     },
     {
-      url: 'https://f1telemetry.vercel.app/schedule',
+      url: 'https://f1telemetry.com/schedule',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
